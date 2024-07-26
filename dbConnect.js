6390d862c0f9a73390d65122ac6f110ce903a547
GRUPO6-3CTJ <3ctjgrupo6@gmail.com>
2024-07-25 22:00:47 -0300

    Add files via upload

--- dbConnect.js
+++ dbConnect.js
@@ -1,8 +1,21 @@
-import mongoose, { mongo } from "mongoose";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function conectaNaDatabase() {
-  mongoose.connect("mongodb+srv://3ctjgrupo6:Fiaptech2024@cluster0.pwplit6.mongodb.net/livraria?retryWrites=true&w=majority&appName=Cluster0");
+  const uri = process.env.MONGODB_URI;
+
+  if (!uri) {
+    throw new Error("A variável de ambiente MONGODB_URI não está definida.");
+  }
+
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+
   return mongoose.connection;
-};
+}
 
 export default conectaNaDatabase;
